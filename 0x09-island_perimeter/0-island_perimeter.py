3260aa7d0d4251163fece6e3f8b008f420d638e7
Salomon700 <ntwalisolomon9@gmail.com>
2024-12-05 01:27:05 +0200

    perimeter of the island described in grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/python3
+"""Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island
+    Args:
+        grid(list): list of list of integers
+    Return:
+        the perimeter of the island
+    """
+
+    param = 0
+
+    for idx in range(len(grid)):
+        for j in range(len(grid[idx])):
+            if (grid[idx][j] == 1):
+                if (idx <= 0 or grid[idx - 1][j] == 0):
+                    param += 1
+                if (idx >= len(grid) - 1 or grid[idx + 1][j] == 0):
+                    param += 1
+                if (j <= 0 or grid[idx][j - 1] == 0):
+                    param += 1
+                if (j >= len(grid[idx]) - 1 or grid[idx][j + 1] == 0):
+                    param += 1
+    return param
